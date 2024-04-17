fb14daa7c100ce435cfcd806d7bbf99cddc95cab
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2024-04-17 17:16:32 +0000

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,9 +11,9 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'Mikah Yobrats', // Site author
 	title: 'Error Bitz', // Site title.
-	description: 'Tech Blog with error solving capabilities', // Description to display in the meta tags
+	description: 'Error Bitz (Bits).Stay updated on the latest in technology while finding solutions to your code errors. Discover articles, tips, and tricks to conquer tech challenges.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this error fix', // Message to share a post on social media
+	shareMessage: 'Share this useful post', // Message to share a post on social media
 	paginationSize: 100 // Number of posts per page
 }
