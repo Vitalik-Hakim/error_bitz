0012ca00a8263f01485022258967bb42cb884792
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2024-04-19 02:52:26 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://errorbitz.netlify.app/', // Write here your website url
+	site: 'https://errorbitz.onlynewsgh.com/', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
